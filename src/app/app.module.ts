c28f6741dbb37a9b03c49cd73671b1d54c412b51
Bashi <S_Bowerman925@yahoo.com>
2018-07-01 17:41:31 -0700

    Created Angular file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MapsComponent } from './maps/maps.component';
+import { BrandsComponent } from './brands/brands.component';
+import { DealsComponent } from './deals/deals.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {path: '', component:HomeComponent},
+  {path: 'maps', component:MapsComponent},
+  {path: 'deals', component:DealsComponent},
+  {path: 'brands', component:BrandsComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MapsComponent,
+    BrandsComponent,
+    DealsComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
